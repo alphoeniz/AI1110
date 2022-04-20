5e59d6b3e7dd63e5a781e7d4bb19840412871b2d
Varun Gupta <thecodervarun@gmail.com>
2022-04-20 10:08:13 +0530

    Initial commit

--- Assignment-2/codes/fig.py
+++ Assignment-2/codes/fig.py
@@ -1,48 +1,42 @@
 # ICSE 2018 Grade 12 22
 # Varun Gupta (cs21btech11060)
 
-# This code shades feasible region for the solution.  
+# Plotting the feasible region for the solution.
 
-# Python libraries for math and graphics
 import matplotlib.pyplot as plt
 import numpy as np
-from params import *
 
-#3x+4y=90
+plt.xlim(-2, 25)
+plt.ylim(-2, 15)
 
-def line_gen(A,B):
-  len =10
-  dim = A.shape[0]
-  x_AB = np.zeros((dim,len))
-  lam_1 = np.linspace(0,1,len)
-  for i in range(len):
-    temp1 = A + lam_1[i]*(B-A)
-    x_AB[:,i]= temp1.T
-  return x_AB
+def f(x):
+    return (60-3*x)/4
+def g(x):
+    return (30-x)/3
 
-#Intersection of two lines
-def line_intersect(n1,A1,n2,A2):
-  N=np.vstack((n1,n2))
-  print(type(N))
-  p = np.zeros(2)
-  p[0] = n1@A1
-  p[1] = n2@A2
-  #Intersection
-  P=np.linalg.inv(N)@p
-  return P
+fxlist = np.linspace(12,20,num = 1000)
+fylist = f(fxlist)
+gxlist = np.linspace(0,12,num = 1000)
+gylist = g(gxlist)
 
-def norm_vec(A,B):
-  return np.matmul(omat, dir_vec(A,B))
+plt.plot(fxlist, fylist, color = "r", label = "$9x+12y=180$")
+plt.plot(gxlist, gylist, color = "b", label = "$x+3y=30$")
+plt.plot([0,0], [0,10], color = "g")
+plt.plot([0,20], [0,0], color = "orange")
 
-def plt_point(A):
-  plt.plot(A[0],A[1],marker = "o", markersize = 3, color = 'black')
+plt.text(-1,-1,"$P_3$")
+plt.text(20,-1,"$P_4$")
+plt.text(0,10.5,"$P_2$")
+plt.text(12.25,6.25,"$P_1$")
 
-plt.axes()
-plt.xlim(0, 25)
-plt.ylim(0, 25);
+plt.fill_between(fxlist, fylist, 0, where = (fxlist > 12) & (fxlist <= 20), color = '#00FFBF', label = 'Feasible Region')
+plt.fill_between(gxlist, gylist, 0, where = (gxlist > 0) & (gxlist <= 12), color = '#00FFBF')
 
-plt.ylabel('Y-axis')
-plt.xlabel('X-axis')
-plt.legend(loc = 'best')
+xpoints = [0,20,0,12]
+ypoints = [0,0,10,6]
+plt.scatter(xpoints,ypoints, s=40, marker="o", color='cyan')
+
+plt.legend(loc="best")
+
 plt.grid()
 plt.show()
