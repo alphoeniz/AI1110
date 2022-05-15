9ca0b4e820be18360a286919d2405a0e10245c73
Varun <thecodervarun@gmail.com>
2022-05-16 03:06:53 +0530

    Initial commit

---
+++ Assignment-5/codes/sol.py
@@ -0,0 +1,7 @@
+P_A = 1/2
+P_B = 7/12
+P_nor_A_or_nor_B = 1/4
+P_A_intersection_B = 1 - P_nor_A_or_nor_B
+if P_A*P_B == P_A_intersection_B:
+    print("A and B are independent events")
+print("A and B are not independent events")
